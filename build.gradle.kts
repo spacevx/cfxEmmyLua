12a21c1669735f811176c971947e58b2c30eda72
Space V <40030799+spacevx@users.noreply.github.com>
2024-12-06 00:23:49 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,7 +43,7 @@ val buildDataList = listOf(
         ideaSDKShortVersion = "231",
         ideaSDKVersion = "2023.1",
         sinceBuild = "231",
-        untilBuild = "242.*"
+        untilBuild = "243.*"
     ),
     BuildData(
         ideaSDKShortVersion = "223",
